207071bedbf0d8def63364b0a63c2804b8426e18
Shivam Kumar <shivamkumar101202@gmail.com>
2024-09-30 10:24:19 +0530

    Add files via upload

---
+++ mathematical.py
@@ -0,0 +1,15 @@
+#Mathematical function (+, -, *, /)
+n_one=int(input("Enter The Value Of N_1 : "))
+n_two=int(input("Enter The Value Of N_2 : "))
+
+Add=n_one + n_two
+print("Addition is : ",Add)
+
+Sub=n_one - n_two
+print("Subtraction is : ",Sub)
+
+Mul=n_one * n_two
+print("Multiply is : ",Mul)
+
+Div=n_one / n_two
+print("Division is : ",Div)
